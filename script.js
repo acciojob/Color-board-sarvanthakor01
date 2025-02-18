82396c5409789ed25b09e447e37ad4e1d2a88b0b
Sarvan Thakor <97446457+sarvanthakor01@users.noreply.github.com>
2025-02-19 00:17:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const colorBoard = document.getElementById('color-board');
+
+// Create 800 boxes
+for (let i = 0; i < 800; i++) {
+    const box = document.createElement('div');
+    box.classList.add('square');
+    colorBoard.appendChild(box);
+}
